891deafb78254f7b0784ed37451d25ebe8cfb6f1
Gali Katz <gali.k@taboola.com>
2019-04-03 22:25:49 +0300

    recording endpoint

---
+++ frontend/src/main/java/com/taboola/hackathon/frontend/speech_to_text/SpeechToTextConverter.java
@@ -0,0 +1,59 @@
+package com.taboola.hackathon.frontend.speech_to_text;// Imports the Google Cloud client library
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SpeechToTextConverter {
+    private SpeechClient speechClient;
+    public SpeechToTextConverter(){
+      try {
+        this.speechClient = SpeechClient.create();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public List<SpeechRecognitionResult> speechToText(String fileName) throws IOException {
+      // Reads the audio file into memory
+      Path path = Paths.get(fileName);
+      byte[] data = Files.readAllBytes(path);
+      ByteString audioBytes = ByteString.copyFrom(data);
+
+      // Builds the sync recognize request
+      RecognitionConfig config = RecognitionConfig.newBuilder()
+              .setEncoding(AudioEncoding.LINEAR16)
+              .setSampleRateHertz(16000)
+              .setLanguageCode("en-US")
+              .build();
+      RecognitionAudio audio = RecognitionAudio.newBuilder()
+              .setContent(audioBytes)
+              .build();
+
+      // Performs speech recognition on the audio file
+      RecognizeResponse response = speechClient.recognize(config, audio);
+      List<SpeechRecognitionResult> results = response.getResultsList();
+      return results;
+    }
+
+  public static void main(String... args) throws Exception {
+    SpeechToTextConverter speechToTextConverter = new SpeechToTextConverter();
+    List<SpeechRecognitionResult> speechRecognitionResults = speechToTextConverter.speechToText("./resources/audio.raw");
+    for (SpeechRecognitionResult result : speechRecognitionResults) {
+      // There can be several alternative transcripts for a given chunk of speech. Just use the
+      // first (most likely) one here.
+      SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+      System.out.printf("Transcription: %s%n", alternative.getTranscript());
+    }
+  }
+}
